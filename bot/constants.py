bfda2b1f67c2f8b5d2cc6d726ee0d725a0f6d404
DHECK17 <81514501+DHECK17@users.noreply.github.com>
2021-06-13 14:50:06 +0530

    Update constants.py

--- bot/constants.py
+++ bot/constants.py
@@ -195,32 +195,4 @@ LANGUAGES = {
 }
 
 
-@dataclass
-class Emoji:
-    """
-    Represents storage for custom and external emojis.
-    """
-    class Workers:
-        """
-        Represents emojis for workers health check.
-        (command in bot.cogs.information)
-        """
-        total= "<:total:620744869429641236>"
-        available = "<:available:620705066604560405>"
-        idle = "<:idle:620702759414661120>"
-        working = "<:working:620704067672342528>"
-        paused = "<:paused:620704067479666688>"
-        failed = "<:failed:620704067525672980>"
 
-    class Execution:
-        loading = "<a:typing:705421984141672470>"
-        error = "<:dnd:705421983952666674>"
-        successful = "<:online:705421983927763055>"
-        offline = "<:offline:705421983873105920>"
-        idle = "<:idle:705421983906660454>"
-
-@dataclass
-class Color:
-    difficulties = [
-5025872, 9225035, 13491257, 16772154, 16761352, 16750593, 16668450, 16073527
-]
