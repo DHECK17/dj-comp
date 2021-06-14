f674e268ac6b3dd846e8095095da82f8d7b1686e
DHECK17 <81514501+DHECK17@users.noreply.github.com>
2021-06-14 14:47:13 +0530

    Update constants.py

--- bot/constants.py
+++ bot/constants.py
@@ -45,139 +45,139 @@ LANGUAGES = {
         'aliases': ['sh'],
         'version': 'Bash (4.4)',
         'icon': 'https://i.imgur.com/6BQ4g4J.png',
-        'emoji': '<:weary:>',
+        ,
         },
     50: {
         'command': 'c',
         'version': 'C (GCC 9.2.0)',
         'icon': 'https://i.imgur.com/Aoo95wm.png',
-        'emoji': '<:weary:>',
+        
         },
     54: {
         'command': 'cpp',
         'version': 'C++ (GCC 9.2.0)',
         'aliases': ['c++'],
         'icon': 'https://i.imgur.com/CJYqkG5.png',
-        'emoji': '<:weary:>',
+       
         },
     51: {
         'command': 'csharp',
         'version': 'C# (Mono 6.6.0.161)',
         'aliases': ['c#'],
         'icon': 'https://i.imgur.com/M1AQVY2.png',
-        'emoji': '<:weary:>',
+        
         },
     55: {
         'command': 'lisp',
         'version': 'Common Lisp (SBCL 2.0.0)',
         'icon': 'https://i.imgur.com/ms9qW6e.png',
-        'emoji': '<:weary:>',
+       
         },
     56: {
         'command': 'd',
         'version': 'D (DMD 2.089.1)',
         'icon': 'https://i.imgur.com/uoeBAsf.png',
-        'emoji': '<:weary:>',
+       
         },
     57: {
         'command': 'elixir',
         'version': 'Elixir (1.9.4)',
         'icon': 'https://i.imgur.com/0tigIIL.png',
-        'emoji': '<:weary:>',
+        
         },
     58: {
         'command': 'erlang',
         'version': 'Erlang (OTP 22.2)',
         'icon': 'https://i.imgur.com/5dVX2NF.png',
-        'emoji': '<:weary:>',
+        
         },
     60: {
         'command': 'go',
         'version': 'Go (1.13.5)',
         'aliases': ['golang'],
         'icon': 'https://i.imgur.com/a3yrHtU.png',
-        'emoji': '<:weary:>',
+       
         },
     61: {
         'command': 'haskell',
         'version': 'Haskell (GHC 8.8.1)',
         'icon': 'https://i.imgur.com/NpyZ3z7.png',
-        'emoji': '<:weary:>',
+       
         },
     62: {
         'command': 'java',
         'version': 'Java (OpenJDK 13.0.1)',
         'icon': 'https://i.imgur.com/5OwBztX.png',
-        'emoji': '<:weary:>',
+        
         },
     63: {
         'command': 'javascript',
         'aliases': ['js'],
         'version': 'JavaScript (Node.js 12.14.0)',
         'icon': 'https://i.imgur.com/YOsQqBF.png',
-        'emoji': '<:weary:>',
+       
         },
     64: {
         'command': 'lua',
         'version': 'Lua (5.3.5)',
         'icon': 'https://i.imgur.com/WVKZnEo.png',
-        'emoji': '<:weary:>',
+        
         },
     65: {
         'command': 'ocaml',
         'version': 'OCaml (4.09.0)',
         'icon': 'https://i.imgur.com/pKLADe6.png',
-        'emoji': '<:weary:>',
+       
         },
     66: {
         'command': 'octave',
         'version': 'Octave (5.1.0)',
         'icon': 'https://i.imgur.com/dPwBc2g.png',
-        'emoji': '<:weary:>',
+        
         },
     67: {
         'command': 'pascal',
         'version': 'Pascal (FPC 3.0.4)',
         'icon': 'https://i.imgur.com/KjSF3JE.png',
-        'emoji': '<:weary:>',
+        
         },
     68: {
         'command': 'php',
         'version': 'PHP (7.4.1)',
         'icon': 'https://i.imgur.com/cnnYSIE.png',
-        'emoji': '<:weary:>',
+        
         },
     69: {
         'command': 'prolog',
         'version': 'Prolog (GNU Prolog 1.4.5)',
         'icon': 'https://i.imgur.com/yQAknfK.png',
-        'emoji': '<:weary:>',
+        
         },
     71: {
         'command': 'python',
         'version': 'Python (3.8.1)',
         'aliases': ['py'],
         'icon': 'https://i.imgur.com/N4RyEvG.png',
-        'emoji': '<:weary:>',
+       
         },
     72: {
         'command': 'ruby',
         'version': 'Ruby (2.7.0)',
         'icon': 'https://i.imgur.com/u9xb12N.png',
-        'emoji': '<:weary:>',
+        
         },
     73: {
         'command': 'rust',
         'version': 'Rust (1.40.0)',
         'icon': 'https://i.imgur.com/l1TnRxU.png',
-        'emoji': '<:weary:>',
+        
         },
     74: {
         'command': 'typescript',
         'aliases': ['ts'],
         'version': 'TypeScript (3.7.4)',
         'icon': 'https://i.imgur.com/IBjXVQv.png',
-        'emoji': '<:weary:>',
+        
         }
    },
    #TODO make this dictionary based on "array"
